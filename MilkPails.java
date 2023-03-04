16fdd2c461d67bbd3a1946d09060a94711d6af48
heimlichmaneuve <97202637+heimlichmaneuve@users.noreply.github.com>
2023-03-04 00:35:20 -0800

    Made algorithm more efficient

--- MilkPails.java
+++ MilkPails.java
@@ -5,25 +5,21 @@ import java.util.Scanner;
 
 public class MilkPails {
     public static void main(String[] args) throws Exception {
-        
+
         Scanner input = new Scanner(new File("pails.in"));
-        String[] XYM = (input.nextLine()).split(" ");
+        String[] XYM = input.nextLine().split(" ");
         int X = Integer.parseInt(XYM[0]);
         int Y = Integer.parseInt(XYM[1]);
         int M = Integer.parseInt(XYM[2]);
         int a = M/X;
-        int b = M/Y;
+        int aX = 0;
         int maxMilk = 0;
 
-        for (int f = 0; f <= a; f++) {
-            for (int i = 0; i <= b; i++) {
-                int aX = (f)*(X);
-                int bY = (i)*(Y);
-                if (((aX) + (bY)) <= M) {
-                    if (((aX) + (bY)) > maxMilk) {
-                        maxMilk = aX + bY;
-                    }
-                }
+        for (int f = 0; f <= a && maxMilk < M; f++) {
+            aX = f*X;
+            int currentMaxMilk = aX + (Y)*((M - aX)/Y);
+            if (currentMaxMilk > maxMilk) {
+                maxMilk = currentMaxMilk;
             }
         }
 
